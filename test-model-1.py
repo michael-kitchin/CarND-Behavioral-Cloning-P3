1215e6a8540afe0eedd14b45c44259b87b82b5f9
Michael J. Kitchin <michael.kitchin@opsysinc.com>
2018-01-25 15:24:37 -0800

    Updated.

---
+++ test-model-1.py
@@ -0,0 +1,32 @@
+import csv
+import cv2
+import numpy as np
+
+lines = []
+with open('./input_data/driving_log.csv') as csvfile:
+    reader = csv.read(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = './input_data/IMG/' + filename
+    image = cv2.imread(current_path)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+
+model = Sequential()
+model.add(Flatten(input_shape=(160,320,3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+
+model.save('model.h5')
