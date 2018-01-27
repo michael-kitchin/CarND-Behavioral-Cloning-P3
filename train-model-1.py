fa8a24d7361283866ad457e00c3bcac117131da6
Michael J. Kitchin <mcoyote@mcoyote.com>
2018-01-26 17:07:14 -0700

    Updated.

--- train-model-1.py
+++ train-model-1.py
@@ -28,13 +28,24 @@ y_train = np.array(measurements)
 print ('y_train',len(y_train))
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Dropout, Lambda, Conv2D, MaxPooling2D
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Conv2D(6,(5,5),activation="relu"))
+model.add(MaxPooling2D())
+model.add(Conv2D(6,(5,5),activation="relu"))
+model.add(MaxPooling2D())
+# model.add(Dropout(0.25))
+model.add(Flatten())
+# model.add(Dropout(0.5))
+model.add(Dense(120))
+# model.add(Dropout(0.5))
+model.add(Dense(84))
+model.add(Dropout(0.5))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=2)
 
 model.save('model.h5')
